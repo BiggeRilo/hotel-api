15dd7d5fa370455779fccd8b7ef7c5bf04d114ac
mubenassir <mubenassi@outlook.com>
2025-05-12 13:54:22 -0300

    Project created

---
+++ HotelAPI/src/test/java/com/mubenassir/hotel/api/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mubenassir.hotel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
